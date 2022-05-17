521cae09387c35e9e1a77e963495c7080f40e1aa
michallides-MSI <wholemichal@gmail.com>
2022-05-17 15:51:06 +0200

    todays commit

--- script.js
+++ script.js
@@ -1,38 +1,16 @@
 'use strict';
 
-const player = document.querySelector('.player');
-const player1 = document.querySelector('.player-1');
-const player2 = document.querySelector('.player-2');
-const score1 = document.querySelector('.score-1');
-const score2 = document.querySelector('.score-2');
-const currentScore1 = document.querySelector('.current-score-1');
-const currentScore2 = document.querySelector('.current-score-2');
+const player0 = document.querySelector('#player-0');
+const player1 = document.querySelector('#player-1');
+const score0 = document.querySelector('#score-0');
+const score1 = document.querySelector('#score-1');
+const currentScore0 = document.querySelector('#current-score-0');
+const currentScore1 = document.querySelector('#current-score-1');
+
+const playerActive = document.querySelector('.player-active');
 const currentActive = document.querySelector('.current-active');
-const newGame = document.querySelector('.new-game-btn');
-const rollDice = document.querySelector('.roll-dice-btn');
-const hold = document.querySelector('.hold-btn');
+
+const newGameBtn = document.querySelector('.new-game-btn');
+const rollDiceBtn = document.querySelector('.roll-dice-btn');
+const holdBtn = document.querySelector('.hold-btn');
 const dice = document.querySelector('.dice');
-let currentScore = Number(currentActive.textContent);
-
-rollDice.addEventListener('click', function () {
-  let number = Math.trunc(Math.random() * 6) + 1;
-  dice.src = `img/dice-${number}.svg`;
-
-  if (number === 1) {
-    currentScore = 0;
-    currentActive.textContent = currentScore;
-
-    if (
-      currentScore1.classList.contains('current-active') &&
-      player1.classList.contains('player-active')
-    ) {
-      currentScore1.classList.remove('current-active');
-      currentScore2.classList.add('current-active');
-      player1.classList.remove('player-active');
-      player2.classList.add('player-active');
-    }
-  } else {
-    currentScore += number;
-    currentActive.textContent = currentScore;
-  }
-});
