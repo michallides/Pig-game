64a0ac0b013d6db433ecda246910ea0a3cb66557
michallides-MSI <wholemichal@gmail.com>
2022-05-25 15:35:12 +0200

    todays commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// GLOBAL VARIABLES
 const player0 = document.querySelector('.player-0');
 const player1 = document.querySelector('.player-1');
 const score0 = document.querySelector('#score-0');
@@ -12,11 +13,12 @@ const currentBox1 = document.querySelector('.current-box-1');
 const winnerImg0 = document.querySelector('.winner-img-0');
 const winnerImg1 = document.querySelector('.winner-img-1');
 const diceEl = document.querySelector('.dice');
+const buttons = document.querySelectorAll('.btn');
 const newGameBtn = document.querySelector('.new-game-btn');
 const rollDiceBtn = document.querySelector('.roll-dice-btn');
 const holdBtn = document.querySelector('.hold-btn');
 
-let scores, currentScore, playerActive, playing;
+let scores, currentScore, playerActive, playing, rolling;
 
 // STARTING CONDITIONS FUNCTION
 const init = function () {
@@ -24,6 +26,7 @@ const init = function () {
   currentScore = 0;
   playerActive = 0;
   playing = true;
+  rolling = false;
 
   score0.textContent = 0;
   score1.textContent = 0;
@@ -56,10 +59,18 @@ const switchPlayer = function () {
 
 // ROLL DICE BTN FUNCTIONALITY
 rollDiceBtn.addEventListener('click', function () {
-  if (playing) {
+  if (playing && !rolling) {
+    rolling = true;
     const diceNum = Math.trunc(Math.random() * 6) + 1;
     diceEl.classList.remove('hidden');
-    diceEl.src = `img/dice-${diceNum}.svg`;
+    diceEl.classList.add('rotating');
+    diceEl.src = `img/dice-0.svg`;
+
+    setTimeout(function () {
+      diceEl.classList.remove('rotating');
+      diceEl.src = `img/dice-${diceNum}.svg`;
+      rolling = false;
+    }, 700);
 
     if (diceNum !== 1) {
       currentScore += diceNum;
@@ -107,3 +118,13 @@ holdBtn.addEventListener('click', function () {
 
 // NEW GAME BTN FUNCTIONALITY
 newGameBtn.addEventListener('click', init);
+
+// ROTATING BTN ICONS FUNCTIONALITY
+buttons.forEach(btn =>
+  btn.addEventListener('click', function () {
+    this.classList.add('rotating');
+    setTimeout(function () {
+      btn.classList.remove('rotating');
+    }, 700);
+  })
+);
