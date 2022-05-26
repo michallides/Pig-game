c9a500e9549e98a6eeee4e9a0b4827dd1d2c0036
michallides-MSI <wholemichal@gmail.com>
2022-05-26 15:09:41 +0200

    todays commit

--- script.js
+++ script.js
@@ -10,14 +10,15 @@ const currentScore1 = document.querySelector('#current-score-1');
 const currentBox0 = document.querySelector('.current-box-0');
 const currentBox1 = document.querySelector('.current-box-1');
 
+const diceEl = document.querySelector('.dice');
 const winnerImg0 = document.querySelector('.winner-img-0');
 const winnerImg1 = document.querySelector('.winner-img-1');
-const diceEl = document.querySelector('.dice');
 const buttons = document.querySelectorAll('.btn');
 const newGameBtn = document.querySelector('.new-game-btn');
 const rollDiceBtn = document.querySelector('.roll-dice-btn');
 const holdBtn = document.querySelector('.hold-btn');
 
+let winningScore = 5;
 let scores, currentScore, playerActive, playing, rolling;
 
 // STARTING CONDITIONS FUNCTION
@@ -39,13 +40,21 @@ const init = function () {
   player0.classList.remove('player-winner');
   player1.classList.remove('player-winner');
   newGameBtn.classList.remove('highlighted');
-  winnerImg0.classList.add('hidden');
-  winnerImg1.classList.add('hidden');
-  winnerImg0.classList.remove('fade-in');
-  winnerImg1.classList.remove('fade-in');
+  rollDiceBtn.classList.remove('hidden');
+  holdBtn.classList.remove('hidden');
   currentBox0.classList.remove('hidden');
   currentBox1.classList.remove('hidden');
+  score0.classList.remove('hidden');
+  score1.classList.remove('hidden');
+
+  winnerImg0.classList.add('hidden');
+  winnerImg1.classList.add('hidden');
+  setTimeout(function () {
+    winnerImg0.classList.add('removed');
+    winnerImg1.classList.add('removed');
+  }, 500);
 };
+
 init();
 
 // SWITCH PLAYER FUNCTION
@@ -64,7 +73,7 @@ rollDiceBtn.addEventListener('click', function () {
     const diceNum = Math.trunc(Math.random() * 6) + 1;
     diceEl.classList.remove('hidden');
     diceEl.classList.add('rotating');
-    diceEl.src = `img/dice-0.svg`;
+    diceEl.src = 'img/dice-0.svg';
 
     setTimeout(function () {
       diceEl.classList.remove('rotating');
@@ -84,14 +93,22 @@ rollDiceBtn.addEventListener('click', function () {
 
 // HOLD BTN FUNCTIONALITY
 holdBtn.addEventListener('click', function () {
+  let scoreActive = document.querySelector(`#score-${playerActive}`);
+  let winnerImg = document.querySelector(`.winner-img-${playerActive}`);
+
   if (playing) {
     scores[playerActive] += currentScore;
-    document.querySelector(`#score-${playerActive}`).textContent =
-      scores[playerActive];
+    scoreActive.textContent = scores[playerActive];
 
-    if (scores[playerActive] >= 5) {
+    if (scores[playerActive] >= winningScore) {
       playing = false;
       diceEl.classList.add('hidden');
+      holdBtn.classList.add('hidden');
+      rollDiceBtn.classList.add('hidden');
+
+      scoreActive.classList.add('hidden');
+
+      newGameBtn.classList.add('highlighted');
 
       document
         .querySelector(`.player-${playerActive}`)
@@ -101,15 +118,12 @@ holdBtn.addEventListener('click', function () {
         .querySelector(`.current-box-${playerActive}`)
         .classList.add('hidden');
 
-      document
-        .querySelector(`.winner-img-${playerActive}`)
-        .classList.remove('hidden');
+      winnerImg.classList.remove('removed');
 
-      document
-        .querySelector(`.winner-img-${playerActive}`)
-        .classList.add('fade-in');
-
-      newGameBtn.classList.add('highlighted');
+      // don't need time, because 'hidden' has transition-duratinon in CSS
+      setTimeout(function () {
+        winnerImg.classList.remove('hidden');
+      });
     } else {
       switchPlayer();
     }
@@ -120,11 +134,12 @@ holdBtn.addEventListener('click', function () {
 newGameBtn.addEventListener('click', init);
 
 // ROTATING BTN ICONS FUNCTIONALITY
-buttons.forEach(btn =>
+buttons.forEach(btn => {
   btn.addEventListener('click', function () {
-    this.classList.add('rotating');
+    btn.classList.add('rotating');
+
     setTimeout(function () {
       btn.classList.remove('rotating');
     }, 700);
-  })
-);
+  });
+});
